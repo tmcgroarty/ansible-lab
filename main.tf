0625ed0a39134c70b56b991c1d0138d9f7f1f433
timothy mcgroarty <tim.mcgroarty@riverbed.com>
2026-04-19 22:48:16 +0000

    update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
-" {
+provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "ansible" {
-  ami           = "ami-xxxxxxxxxxxxxxxxx"
-  instance_type = "t2.micro"
+resource "aws_instance" "ubuntu_server" {
+  ami                    = "ami-0d1b5a8c13042c939"   # Ubuntu 24.04 LTS in us-east-2
+  instance_type          = "t2.micro"
+  key_name               = "OHIO"
+  vpc_security_group_ids = ["sg-04d4338bc7e62a46b"]
 
   tags = {
-    Name = "Ansible-Server"
+    Name = "Ubuntu-Server"
   }
 }
