9dd904774f97b44d7db0f968e6c654102fb5afba
timothy mcgroarty <tim.mcgroarty@riverbed.com>
2026-04-21 20:17:42 +0000

    changed main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,19 @@ resource "aws_instance" "ubuntu_server" {
   subnet_id              = "subnet-0a6338b1259311ef9"
   vpc_security_group_ids = ["sg-0915280fa120808de"]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y software-properties-common
+
+              # Add Ansible repository
+              add-apt-repository --yes --update ppa:ansible/ansible
+
+              # Install Ansible
+              apt-get install -y ansible
+              EOF
+
   tags = {
-    Name = "Ubuntu-Server"
+    Name = "Ubuntu-Ansible-Server"
   }
 }
